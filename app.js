4384f8d284e563ef1ee3a688462e3701c7495a22
RoySamson-stack <rsam9009@gmail.com>
2022-01-22 12:21:02 +0300

    adding the get and post request to the https server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const https = require("https");
 const express = require("express");
+const fs = require("fs");
 // const ejs = require("ejs");
 
 const app = express();
@@ -14,8 +15,26 @@ if (port == null || port == "") {
 }
 
 const server = https.createServer(function(req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" , "Content-Length": "0"});
-  res.write("Hello World!");
-  res.end();
-}).listen(port);
+  var reqUrlString = req.url;
+  var urlObject = require("url").parse(reqUrlString, true);
+  var pathname = urlObject.pathname;
+
+
+  var fileName = urlObject.pathname.substring(1);
+  if("POST " == method){
+    req.on('data', fucntion(chunk){
+      console.log(chunk);
+    })
+  }
+
+
+  fs.readFile(fileName, function(err){
+    res.writeHead(200, { "Content-Type": "text/html" , "Content-Length": "100"});
+    res.write("Success GET request");
+    res.end();
+  })
+ 
+})
+
+server.listen(port);
 
