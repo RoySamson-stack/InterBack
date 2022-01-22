576ba41459843873f76ef5816ef30025f6de465b
RoySamson-stack <rsam9009@gmail.com>
2022-01-22 09:41:56 +0300

    adding the server side code for the http server

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const http = require("http");
+
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("Hello World!");
+  res.end();
+}).listen(8080);
