dcd6af93dcbf6bc6d4ceefc03a452b2ca60e69cf
RoySamson-stack <rsam9009@gmail.com>
2022-01-22 14:01:14 +0300

    final touches for the https server

--- app.js
+++ app.js
@@ -13,14 +13,16 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
+//creating the https server
 
 const server = https.createServer(function(req, res) {
   var reqUrlString = req.url;
   var urlObject = require("url").parse(reqUrlString, true);
   var pathname = urlObject.pathname;
 
-
+//having dummy information that would be passed from the front end to the server
   var fileName = urlObject.pathname.substring(1);
+  //this is the POST request that would be sent from the front end
   if("POST " == method){
     var postData = "";
     req.on("data", function(data) {
@@ -31,6 +33,8 @@ const server = https.createServer(function(req, res) {
       var postData = postObject.data;
     });
   }else{
+
+    //this is the GET request that would be sent to the server
     fs.readFile(fileName, function(err){
       res.writeHead(200, { "Content-Type": "text/html" , "Content-Length": "100"});
       res.write("Success GET request");
