2b475b84d7b44b22018ca145dcf99fe0d733f20b
RoySamson-stack <rsam9009@gmail.com>
2022-01-22 09:57:21 +0300

    adding the get and post request to the server

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 
 
 const http = require("http");
+const express = require("express");
+
+const app = express();
 
 
 http.createServer(function(req, res) {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("Hello World!");
   res.end();
-}).listen(8080);
+})
+
+app.get("/", function(req, res) {
+  res.render("")
+})
+app.post("/", (req, res) => {
+
+})
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(err){
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
